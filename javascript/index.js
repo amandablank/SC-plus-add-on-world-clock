7f9303f2b1723885d1e91045a3c67e6bf42d7050
Amanda <amanda.blankenburg@gmail.com>
2023-03-28 17:07:53 +0200

    Changed the font (CSS), deleted old font files, updated locations and added Javascript (function updateTime)

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,35 @@
+function updateTime() {
+  //São Paulo
+  let saoPauloElement = document.querySelector("#sao-paulo");
+  let saoPauloDateElement = saoPauloElement.querySelector(".date");
+  let saoPauloTimeElement = saoPauloElement.querySelector(".time");
+  let saoPauloTime = moment().tz("America/Sao_Paulo");
 
+  saoPauloDateElement.innerHTML = saoPauloTime.format("dddd, MMMM Do, YYYY");
+  saoPauloTimeElement.innerHTML = saoPauloTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  //Athens
+  let athensElement = document.querySelector("#athens");
+  let athensDateElement = athensElement.querySelector(".date");
+  let athensTimeElement = athensElement.querySelector(".time");
+  let athensTime = moment().tz("Europe/Athens");
+
+  athensDateElement.innerHTML = athensTime.format("dddd, MMMM Do, YYYY");
+  athensTimeElement.innerHTML = athensTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  //Tokyo
+  let tokyoElement = document.querySelector("#tokyo");
+  let tokyoDateElement = tokyoElement.querySelector(".date");
+  let tokyoTimeElement = tokyoElement.querySelector(".time");
+  let tokyoTime = moment().tz("Asia/Tokyo");
+
+  tokyoDateElement.innerHTML = tokyoTime.format("dddd, MMMM Do, YYYY");
+  tokyoTimeElement.innerHTML = tokyoTime.format("h:mm:ss [<small>]A[</small>]");
+}
+
+updateTime();
+setInterval(updateTime, 1000);
